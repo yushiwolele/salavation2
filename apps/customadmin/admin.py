9f85c8eee2f106ff556e1946a70d870c243a7987
xiexiong <17343103905@163.com>
2024-06-10 14:56:42 +0800

    test

--- apps/customadmin/admin.py
+++ apps/customadmin/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import RunningProcessInfo,SystemContent,FileType
 
-#thisisatest
+
+#1
 @admin.register(RunningProcessInfo)
 class CustomAdminAdmin(admin.ModelAdmin):
     """
